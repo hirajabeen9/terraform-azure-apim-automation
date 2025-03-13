9d78a17e8c0983512b37d5bfc14f5c859ea3e751
hirajabeen9 <hirajabeen788@gmail.com>
2025-03-14 00:41:06 +0500

    apim automated by terraform

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -3,29 +3,139 @@ resource "azurerm_api_management_api" "api" {
   resource_group_name = var.rg_name
   api_management_name = var.apim_name
   revision            = "1"
+  display_name        = "My API"  # Add this line
+  protocols           = ["https"] # Add this line
 }
 
-resource "azurerm_api_management_api_operation" "operation" {
+
+# GET Operation
+resource "azurerm_api_management_api_operation" "get_operation" {
+  operation_id        = "get-operation"
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  display_name        = "GET Resource"
+  method              = "GET"
+  url_template        = "/resource"
+}
+
+# POST Operation
+resource "azurerm_api_management_api_operation" "post_operation" {
+  operation_id        = "post-operation"
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  display_name        = "POST Resource"
+  method              = "POST"
+  url_template        = "/resource"
+}
+
+# PUT Operation
+resource "azurerm_api_management_api_operation" "put_operation" {
+  operation_id        = "put-operation"
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  display_name        = "PUT Resource"
+  method              = "PUT"
+  url_template        = "/resource/{id}"
+}
+
+# DELETE Operation
+resource "azurerm_api_management_api_operation" "delete_operation" {
   operation_id        = "delete-operation"
   api_name            = azurerm_api_management_api.api.name
   api_management_name = var.apim_name
   resource_group_name = var.rg_name
   display_name        = "DELETE Resource"
   method              = "DELETE"
-  url_template        = "/resource"
+  url_template        = "/resource/{id}"
 }
 
-resource "azurerm_api_management_api_operation_policy" "policy" {
-  api_name            = azurerm_api_management_api_operation.operation.api_name
+# Example policy for GET
+resource "azurerm_api_management_api_operation_policy" "get_policy" {
+  api_name            = azurerm_api_management_api_operation.get_operation.api_name
   api_management_name = var.apim_name
   resource_group_name = var.rg_name
-  operation_id        = azurerm_api_management_api_operation.operation.operation_id
+  operation_id        = azurerm_api_management_api_operation.get_operation.operation_id
 
   xml_content = <<XML
 <policies>
   <inbound>
-    <find-and-replace from="xyz" to="abc" />
+    <base />
   </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
+}
+
+# Example policy for POST
+resource "azurerm_api_management_api_operation_policy" "post_policy" {
+  api_name            = azurerm_api_management_api_operation.post_operation.api_name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  operation_id        = azurerm_api_management_api_operation.post_operation.operation_id
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
+}
+
+# Example policy for PUT
+resource "azurerm_api_management_api_operation_policy" "put_policy" {
+  api_name            = azurerm_api_management_api_operation.put_operation.api_name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  operation_id        = azurerm_api_management_api_operation.put_operation.operation_id
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
+}
+
+# Example policy for DELETE
+resource "azurerm_api_management_api_operation_policy" "delete_policy" {
+  api_name            = azurerm_api_management_api_operation.delete_operation.api_name
+  api_management_name = var.apim_name
+  resource_group_name = var.rg_name
+  operation_id        = azurerm_api_management_api_operation.delete_operation.operation_id
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
 </policies>
 XML
 }
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,7 +1,11 @@
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_name
+  name                     = "mystorage${random_id.storage.hex}" # Change this line
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "random_id" "storage" {
+  byte_length = 4
 }
