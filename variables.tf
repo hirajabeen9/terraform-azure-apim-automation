363293e547632b31690f50c77d7c5a26082da9da
hirajabeen9 <hirajabeen788@gmail.com>
2025-03-13 23:26:51 +0500

    apim automated by terraform

--- variables.tf
+++ variables.tf
@@ -1,7 +1,35 @@
-variable "rg_name" {}
-variable "location" {}
-variable "apim_name" {}
-variable "publisher_name" {}
-variable "publisher_email" {}
-variable "storage_name" {}
-variable "api_name" {}
+variable "rg_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "storage_name" {
+  description = "Storage Account Name"
+  type        = string
+}
+
+variable "apim_name" {
+  description = "API Management Name"
+  type        = string
+}
+
+variable "publisher_name" {
+  description = "Publisher Name for APIM"
+  type        = string
+}
+
+variable "publisher_email" {
+  description = "Publisher Email for APIM"
+  type        = string
+}
+
+variable "api_name" {
+  description = "API Name"
+  type        = string
+}
